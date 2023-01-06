a54caee3cc802939973cf516ffc9970626910e4b
Ruth Uwemedimo <emailtoruth@virginmedia.com>
2023-01-06 13:48:13 +0000

    reorder head components

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,20 @@
  */
 
 // You can delete this file if you're not using it
+
+var React = require("react");
+
+// Hack, to reorder the helmet components as first in <head> tag
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+    /**
+     * @type {any[]} headComponents
+     */
+    const headComponents = getHeadComponents();
+    headComponents.sort((a, b) => {
+        if (a.props && a.props["data-react-helmet"]) {
+            return 0;
+        }
+        return 1;
+    });
+    replaceHeadComponents(headComponents);
+};
