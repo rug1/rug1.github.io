5a2df1313e12e3ca99b2503e9b0b8f1fe03ee1b8
Ruth Uwemedimo <emailtoruth@virginmedia.com>
2023-01-10 15:10:22 +0000

    Updates

---
+++ component---src-pages-404-js-a0ce5282c0e81130234e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkruth_io=self.webpackChunkruth_io||[]).push([[883],{429:function(e,t,n){n.r(t),n.d(t,{Head:function(){return o}});var u=n(7294),a=n(555),r=n(594);t.default=function(e){return u.createElement(a.Z,{pathname:e.location.pathname},u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))};var o=function(){return u.createElement(r.ZP,{title:"404: Not found",description:"Page not found"})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a0ce5282c0e81130234e.js.map
